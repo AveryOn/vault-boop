99d0945fd3e82c40b0ba78642420741977c7c36e
AveryOn <lnkonion2@gmail.com>
2026-07-12 16:48:18 +0400

    [2026-07-12-1783860498]

---
+++ src/shared/dto/action.dto.ts
@@ -0,0 +1,21 @@
+import z from 'zod'
+import type { actionTable } from '~/server/database/schema'
+
+export type Action = typeof actionTable.$inferSelect
+export type ActionInput = typeof actionTable.$inferInsert
+
+export const createActionDto = z.object({
+  username: z.string().trim().min(3),
+})
+export type CreateActionDto = z.infer<typeof createActionDto>
+export type CreateActionResponse = Action
+
+export const updateActionDto = z.object({
+  username: z.string().trim().min(3).optional(),
+  firstName: z.string().trim().min(3).optional(),
+  lastName: z.string().trim().min(3).optional(),
+  password: z.string().trim().min(8).optional(),
+})
+
+export type UpdateActionDto = z.infer<typeof updateActionDto>
+export type UpdateActionResponse = Action
