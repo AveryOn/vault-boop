e187322275e237deea4d6b77b1b16bc6f716ac0f
AveryOn <lnkonion2@gmail.com>
2026-07-12 17:31:27 +0400

    [2026-07-12-1783863087]

--- src/server/services/admin/cv/skill.service.ts
+++
@@ -1,9 +0,0 @@
-import { cvSkillTable } from '~/server/database/schema'
-import { db } from '~/server/database/client'
-import type { Skill } from '~/shared/dto/cv/skill.dto'
-
-export const CvSkillService = {
-  async getList(): Promise<Skill[]> {
-    return await db.select().from(cvSkillTable)
-  },
-}
--- src/server/services/admin/cv/template.service.ts
+++
@@ -1,9 +0,0 @@
-import { cvTemplateTable } from '~/server/database/schema'
-import { db } from '~/server/database/client'
-import type { Template } from '~/shared/dto/cv/template.dto'
-
-export const CvTemplateService = {
-  async getList(): Promise<Template[]> {
-    return await db.select().from(cvTemplateTable)
-  },
-}
