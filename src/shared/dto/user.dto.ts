7f81f876baf833cd3330048598793a9bc403899e
AveryOn <lnkonion2@gmail.com>
2026-07-12 16:39:48 +0400

    [2026-07-12-1783859988]

--- src/shared/dto/user.dto.ts
+++ src/shared/dto/user.dto.ts
@@ -14,3 +14,12 @@ export type CreateUserDto = z.infer<typeof createUserDto>
 
 export type CreateUserResponse = Omit<User, 'masterPasswordHash' | 'username'>
 
+export const updateUserDto = z.object({
+  username: z.string().trim().min(3).optional(),
+  firstName: z.string().trim().min(3).optional(),
+  lastName: z.string().trim().min(3).optional(),
+  password: z.string().trim().min(8).optional(),
+})
+
+export type UpdateUserDto = z.infer<typeof updateUserDto>
+export type UpdateUserResponse = Omit<User, 'masterPasswordHash' | 'username'>
