34db6f35135789316a42419e53d11ec3f3d9867b
AveryOn <lnkonion2@gmail.com>
2026-07-12 16:26:55 +0400

    [2026-07-12-1783859215]

--- src/server/database/schema/access-token.table.ts
+++ src/server/database/schema/access-token.table.ts
@@ -2,12 +2,12 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { id, timestamp } from '~/server/database/helpers'
 import { userTable } from '~/server/database/schema/user.table'
 
-export const vaultAccessTokenTable = sqliteTable('vault_access_token', {
+export const accessTokenTable = sqliteTable('access_token', {
   id: id(),
 
   token: text('token').notNull(),
 
-  vaultUserId: text('vault_user_id')
+  userId: text('user_id')
     .notNull()
     .references(() => userTable.id, { onDelete: 'cascade' }),
 
