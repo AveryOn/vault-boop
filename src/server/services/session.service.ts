2f91732a62bba3213a1ce6da5d91d2ccc8102606
AveryOn <lnkonion2@gmail.com>
2026-07-12 18:03:05 +0400

    [2026-07-12-1783864985]

---
+++ src/server/services/session.service.ts
@@ -0,0 +1,46 @@
+import type { Session } from "~/shared/dto/session.dto";
+import { db } from "~/server/database/client";
+import { sessionTable } from "../database/schema";
+import { eq } from "drizzle-orm";
+
+
+export const SessionService = {
+  async getList(): Promise<Session[]> {
+    return await db
+      .select()
+      .from(sessionTable)
+  },
+
+  async getById(sessionId: string): Promise<Session | null> {
+    const [session] = await db
+      .select()
+      .from(sessionTable)
+      .where(
+        eq(sessionTable.id, sessionId),
+      )
+
+    return session ?? null
+  },
+
+  async getByUserId(userId: string): Promise<Session | null> {
+    const [session] = await db
+      .select()
+      .from(sessionTable)
+      .where(
+        eq(sessionTable.userId, userId),
+      )
+
+    return session ?? null
+  },
+
+  async getByAccessTokenId(accessTokenId: string): Promise<Session | null> {
+    const [session] = await db
+      .select()
+      .from(sessionTable)
+      .where(
+        eq(sessionTable.accessTokenId, accessTokenId),
+      )
+
+    return session ?? null
+  }
+}
