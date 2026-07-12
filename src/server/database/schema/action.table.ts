65bf44ea2b7cf83f7bbe048197cf11e0d26ea947
AveryOn <lnkonion2@gmail.com>
2026-07-12 16:49:48 +0400

    [2026-07-12-1783860588]

--- src/server/database/schema/action.table.ts
+++ src/server/database/schema/action.table.ts
@@ -1,16 +1,9 @@
 import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { id, timestamp } from '~/server/database/helpers'
-import { userTable } from '~/server/database/schema/user.table'
 
 export const actionTable = sqliteTable('action', {
   id: id(),
-
   name: text('name').notNull(),
-
-  userId: text('user_id')
-    .notNull()
-    .references(() => userTable.id, { onDelete: 'cascade' }),
-
   createdAt: timestamp('created_at', true).notNull(),
   updatedAt: timestamp('updated_at', true).notNull(),
   deletedAt: timestamp('deleted_at'),
