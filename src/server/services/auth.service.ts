8c3f907cff55737930f59b440eb6d23f1ab8cf4b
AveryOn <lnkonion2@gmail.com>
2026-07-19 18:57:28 +0400

    [2026-07-19-1784473048]

--- src/server/services/auth.service.ts
+++ src/server/services/auth.service.ts
@@ -11,49 +11,23 @@ import { UserRepo } from "~/server/repo/user.repo";
 
 export const AuthService = {
 
+  async validateAuthContext(locals: App.Locals) {
+    return await db.transaction(async (tx) => {
+      try {
+        // 1. Проверить существование пользователя
+      }
+      catch (err) {
+        logger.error('Sign-Up Error', { err })
+        throw err
+      }
+
+    })
+  },
+
   async signUp(dto: SignUpDto, logger: Logger) {
     return await db.transaction(async (tx) => {
       try {
-
-        // CHECK USER BY USERNAME
-        logger.info('Find User By Username:: ' + ProcessStatus.PENDING)
-        const existsUser = await UserRepo.getByUsername(dto.username, tx)
-        if (existsUser) {
-          logger.error('Conflict: User with such username already exists')
-          throw new Error('Conflict')
-        }
-        logger.info('Find User By Username:: ' + ProcessStatus.COMPLETE)
-
-
-        // CREATE NEW USER
-        logger.info('Create new user:: ' + ProcessStatus.PENDING)
-        // Создание нового пользователя
-        const newUser = await UserRepo.create({
-          firstName: dto.firstName,
-          lastName: dto.lastName,
-          password: await hashPassword(dto.password),
-          username: dto.username,
-        }, tx)
-        logger.info('Create new user:: ' + ProcessStatus.COMPLETE)
-
-
-        // GET ACTION BY ID
-        logger.info('Get Action by name:: ' + ProcessStatus.PENDING)
-        const action = await ActionRepo.getByName(ActionKey.SessionCreated, tx)
-        if (!action) {
-          logger.error(`Action ${ActionKey.SessionCreated} not found`, { action: ActionKey.SessionCreated })
-          throw new Error(`Action ${action} not found`)
-        }
-        logger.info('Get Action by name:: ' + ProcessStatus.COMPLETE)
-
-        // CREATE NEW USER_ACTION
-        logger.info('Create new User Action:: ' + ProcessStatus.PENDING)
-        await UserActionRepo.create(
-          { userId: newUser.id },
-          { actionId: action.id, comment: ActionKey.SessionCreated },
-          tx,
-        )
-        logger.info('Create new User Action:: ' + ProcessStatus.COMPLETE)
+        //
       }
       catch (err) {
         logger.error('Sign-Up Error', { err })
@@ -66,32 +40,7 @@ export const AuthService = {
   async signIn(dto: SignInDto, logger: Logger) {
     return await db.transaction(async (tx) => {
       try {
-        // Найти пользователя по username
-        logger.info('Find User By Username:: ' + ProcessStatus.PENDING)
-        const user = await UserRepo.getByUsername(dto.username, tx)
-        if (!user) {
-          logger.error('User not found', { status: HttpStatusCode.NotFound })
-          throw new Error('Not Found')
-        }
-        logger.info('Find User By Username:: ' + ProcessStatus.COMPLETE)
-
-        logger.info('Password Check:: ' + ProcessStatus.PENDING)
-        const hashedPassword = await hashPassword(dto.password)
-        if (user.masterPasswordHash !== hashedPassword) {
-          logger.error('Password Check:: ' + ProcessStatus.ERROR, { msg: 'passwords mismatch' })
-          throw 'Unauthorized'
-        }
-        logger.info('Password Check:: ' + ProcessStatus.COMPLETE)
-
-        // SessionUseCase.getSessionByStatus({
-        //   status: SessionStatus.PENDING
-        //   dto
-        // })
-        // FIND SESSION
-        // logger.info('Find user session:: ' + ProcessStatus.PENDING)
-        // const  = await SessionService.getByStatus({
-        //   status: SessionStatus.ACTIVE
-        // }, tx)
+        //
       } catch (err) {
         logger.error('Sign-In Error', { err })
         throw err
--- src/server/services/user.service.ts
+++ src/server/services/user.service.ts
@@ -1,2 +1,21 @@
+import type { Logger } from "~/shared/logger/logger.client"
+import { UserRepo } from "~/server/repo/user.repo"
+import type { DatabaseTransaction } from "~/server/database/client"
+import { HttpStatusCode } from "axios"
 
-export const UserService = {}
+export const UserService = {
+
+  async getByUsername(username: string, tx?: DatabaseTransaction, logger?: Logger) {
+    try {
+      const user = await UserRepo.getByUsername(username, tx)
+      if (!user) {
+        logger?.error('User with such username is not found', { status: HttpStatusCode.NotFound })
+        throw new Error('User with such username is not found')
+      }
+      return user
+    }
+    catch (err) {
+      logger?.error('Get User By Username: ERROR', { err })
+    }
+  },
+}
